eee92ecdfc2aa960455ac5f07c54be95efa71c16
leandrojamir <leandrojamir@hotmail.com>
2023-04-04 14:41:44 -0300

    start project and open PR

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,3 +1,22 @@
+# iniciando projeto
+# 1 - Testar o construtor/inicializador do objeto Produto
+# Ao analisar o código do projeto, você encontrará a classe do objeto produto
+# já implementada neste arquivo:
+# inventory_report/inventory/product.py, a classe Product.
+
+# Para termos confiança em continuar as implementações, precisamos que você
+# implemente o teste, que certifique que o método __init__ da classe Product
+# esta funcionando corretamente.
+
+# O nome deste teste deve ser test_cria_produto,
+# ele deve verificar o correto preenchimento dos seguintes atributos:
+# id (int)
+# nome_da_empresa (string)
+# nome_do_produto (string)
+# data_de_fabricacao (string)
+# data_de_validade (string)
+# numero_de_serie (string)
+# instrucoes_de_armazenamento (string)
 # from inventory_report.inventory.product import Product
 
 
