d625f290c505d8373426bbb77a7dd90c2bc859b2
leandrojamir <leandrojamir@hotmail.com>
2023-04-12 21:46:16 -0300

    feat: req8 testing the individual product report

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -7,27 +7,47 @@
 # que permite alterar a representatividade do objeto, para que sempre que
 # usarmos um print nele, no lugar de endereço de memória, teremos uma String
 # personalizada.
-# Dica: A reimplementação do __repr__ não faz o objeto retornar exatamente uma
-# string, fazer um cast para string, pode te ajudar.
-# Exemplo da frase:
-# O produto farinha fabricado em 01-05-2021 por Farinini com validade até
-# 02-06-2023 precisa ser armazenado ao abrigo de luz.
 # Agora para mantermos uma boa cobertura de testes, precisamos que você
 # implemente o teste.
-# O nome deste teste deve ser test_relatorio_produto, ele deve instanciar um
-# objeto Product e verificar se acessá-lo a frase de retorno esta correta.
 
 # def __repr__(self):
-#     return (
-#         f"O produto {self.nome_do_produto}"
-#         f" fabricado em {self.data_de_fabricacao}"
-#         f" por {self.nome_da_empresa} com validade"
-#         f" até {self.data_de_validade}"
-#         f" precisa ser armazenado {self.instrucoes_de_armazenamento}."
-#     )
+#         return (
+#             f"O produto {self.nome_do_produto}"
+#             f" fabricado em {self.data_de_fabricacao}"
+#             f" por {self.nome_da_empresa} com validade"
+#             f" até {self.data_de_validade}"
+#             f" precisa ser armazenado {self.instrucoes_de_armazenamento}."
+#         )
 
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
+# O nome deste teste deve ser test_relatorio_produto, ele deve instanciar um
+# objeto Product e verificar se acessá-lo a frase de retorno esta correta.
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    repr_product = Product(
+        11,
+        "Titanium Dioxide II",
+        "Target Corporation",
+        "2020-12-08",
+        "2023-12-08",
+        "FR29 5791 5333 58XR G4PR IG28 D08",
+        "instrucao 11",
+    )
+    # Dica: A reimplementação do __repr__ não faz o objeto retornar exatamente
+    # uma string, fazer um cast para string, pode te ajudar.
+    # Exemplo da frase:
+    # O produto farinha fabricado em 01-05-2021 por Farinini com validade até
+    # 02-06-2023 precisa ser armazenado ao abrigo de luz.
+    assert repr(repr_product) == (
+        # f"O produto {self.nome_do_produto}"
+        "O produto Titanium Dioxide II"
+        # f" fabricado em {self.data_de_fabricacao}"
+        " fabricado em 2020-12-08"
+        # f" por {self.nome_da_empresa} com validade"
+        " por Target Corporation com validade"
+        # f" até {self.data_de_validade}"
+        " até 2023-12-08"
+        # f" precisa ser armazenado {self.instrucoes_de_armazenamento}."
+        " precisa ser armazenado instrucao 11."
+    )
